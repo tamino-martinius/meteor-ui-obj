8a037d9cba119e9f5b5b64d708e6b4679dbead2e
Tamino Martinius <dev@zaku.eu>
2014-11-10 10:57:27 +0100

    updated to METEOR@0.9.0

--- package.js
+++ package.js
@@ -1,27 +1,29 @@
 Package.describe({
-    summary: 'Easily create plain objects with computed keys as meteor ui helper.'
+  name: 'zaku:ui-obj',
+  summary: 'Easily create plain objects with computed keys as meteor ui helper.',
+  version: '0.9.0',
+  git: 'https://github.com/Zaku-eu/meteor-ui-obj.git'
 });
- 
-Package.on_use(function (api) {
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0');
   api.use([
-    'obj',
+    'mrt:obj@1.0.0',
     'coffeescript',
     'standard-app-packages'
   ],'client');
- 
-  api.add_files('client.coffee', 'client');
+  api.addFiles('zaku:ui-obj.coffee');
 });
 
-Package.on_test(function (api) {
+Package.onTest(function(api) {
   api.use([
     'coffeescript',
     'standard-app-packages',
-    'ui-obj',
-    'test-helpers',
+    'zaku:ui-obj',
     'tinytest'
   ], 'client');
   api.add_files([
-    'test.html',
-    'test.coffee'
+    'zaku:ui-obj-tests.html',
+    'zaku:ui-obj-tests.coffee'
   ], 'client');
 });
